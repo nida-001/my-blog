7ace3c6e922a46b1f8756a2c7f9759e702d17aa5
nida-001 <nida3001@gmail.com>
2024-03-05 22:38:22 +0900

    github pages周りの設定中

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,15 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Blog`,
+    title: `きょむやま プログラミングブログ`,
     siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss"],
+  pathPrefix: '/my-blog',
 };
 
 export default config;
